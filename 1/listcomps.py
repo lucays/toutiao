f71f9ad9c1b6d5245a7d4d45e2ee958e90d0a546
luca <lucahhai@gmail.com>
2017-12-03 20:04:13 +0800

    add

--- 1/listcomps.py
+++ 1/listcomps.py
@@ -19,9 +19,7 @@ end_2 = clock()
 print(end_2-start_2)
 
 # 在列表推导式中使用if作条件过滤
-
 lst = ['1', 1, '2', 2, 3, '3', 'a', 'str']
-# 把不同类型的元素分开
 
 nums = [i for i in lst if isinstance(i, int)]  # nums: [1, 2, 3]
 print(nums)
@@ -29,5 +27,18 @@ print(nums)
 s = [i for i in lst if isinstance(i, str)]  # s: ['1', '2', '3', 'a', 'str']
 print(s)
 
-# map(), filter()函数
+# filter()函数
+nums = list(filter(lambda x: isinstance(x, int), lst))  # nums: [1, 2, 3]
+print(nums)
 
+# 嵌套循环
+colors = ['black', 'white']
+sizes = ['L', 'XL']
+# pants: [('black', 'L'), ('black', 'XL'), ('white', 'L'), ('white', 'XL')]
+pants = [(color, size) for color in colors for size in sizes]
+print(pants)
+
+# 上下文的同名变量
+x = 'mydata'
+nums = [x for x in lst if isinstance(x, int)]
+print(x)
