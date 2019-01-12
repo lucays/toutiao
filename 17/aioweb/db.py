1bc51732e4c6978ceefa3face5280e5d41c161b4
luca <lucahhai@gmail.com>
2019-01-12 22:26:49 +0800

    rm id limit

--- 17/aioweb/db.py
+++ 17/aioweb/db.py
@@ -16,7 +16,7 @@ async def get_old_urls(app):
     while True:
         async with app['mysql_engine'].acquire() as conn:
             async with conn.cursor() as cur:
-                sql = "select url from crawl_result where link!='' and id>7990000 limit 2000"
+                sql = "select url from crawl_result where link!='' limit 2000"
                 await cur.execute(sql)
                 app['old_urls'] = {url for url, in await cur.fetchall()}  # 注意逗号
         print('get old_urls!')
