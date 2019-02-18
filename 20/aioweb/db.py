b31b0e473270cd055c08b36c0bd08b393251a480
luca <lucahhai@gmail.com>
2019-02-18 23:18:51 +0800

    add

---
+++ 20/aioweb/db.py
@@ -0,0 +1,31 @@
+import asyncio
+
+import aiomysql
+
+
+async def mysql_engine(app):
+    mysql_conf = app['config']['mysql']
+    host, port, user, password, db = mysql_conf['host'], mysql_conf['port'], mysql_conf['user'], mysql_conf['password'], mysql_conf['db']
+    app['mysql_engine'] = await aiomysql.create_pool(host=host, port=port, user=user, password=password, db=db, loop=app.loop)
+    yield
+    app['mysql_engine'].close()
+    await app['mysql_engine'].wait_closed()
+
+
+async def get_old_urls(app):
+    while True:
+        async with app['mysql_engine'].acquire() as conn:
+            async with conn.cursor() as cur:
+                sql = "select url from crawl_result where link!='' limit 200"
+                await cur.execute(sql)
+                app['old_urls'] = {url for url, in await cur.fetchall()}  # 注意逗号
+        print('get old_urls!')
+        interval = float(app['config']['update_interval'])
+        await asyncio.sleep(3600*interval)  # 每隔interval小时运行一次
+
+
+async def background_tasks(app):
+    app['get_old_urls'] = app.loop.create_task(get_old_urls(app))
+    yield
+    app['get_old_urls'].cancel()
+    await app['get_old_urls']
---
+++ 20/aioweb/main.py
@@ -0,0 +1,20 @@
+from aiohttp import web
+import jinja2
+import aiohttp_jinja2
+
+from settings import config
+
+from db import mysql_engine, background_tasks
+from views import routes
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
+    app['static_root_url'] = 'static'
+    app['config'] = config
+    app.cleanup_ctx.append(mysql_engine)
+    app.cleanup_ctx.append(background_tasks)
+    app.add_routes(routes)
+    app.router.add_static('/static/', path='./static', name='static')
+    web.run_app(app)
