69298d72885e96cd3f8f5092adab941e56ebc992
luca <lucahhai@gmail.com>
2019-02-18 23:34:12 +0800

    support css

--- 20/aioweb/main.py
+++ 20/aioweb/main.py
@@ -11,6 +11,7 @@ from views import routes
 if __name__ == '__main__':
     app = web.Application()
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
+    app['name'] = 'myproject'
     app['static_root_url'] = 'static'
     app['config'] = config
     app.cleanup_ctx.append(mysql_engine)
